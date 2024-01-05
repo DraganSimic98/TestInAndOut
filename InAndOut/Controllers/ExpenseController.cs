e2bb3cc3f51db857a64b3648ac4b09178ac7b68d
DraganSimic98 <dragansimic29@gmail.com>
2024-01-05 17:55:06 +0100

    Displaying the types and getting their details via include

--- InAndOut/Controllers/ExpenseController.cs
+++ InAndOut/Controllers/ExpenseController.cs
@@ -23,6 +23,12 @@ namespace InAndOut.Controllers
         public IActionResult Index()
         {
             IEnumerable<Expense> objList = _db.Expenses;
+
+            foreach(var obj in objList)
+            {
+                obj.ExpenseType = _db.ExpenseTypes.FirstOrDefault(u => u.Id == obj.ExpenseTypeId);
+            }
+
             return View(objList);
 
         }
